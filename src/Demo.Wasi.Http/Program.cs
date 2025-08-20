dac8fee88ff0b33c496ef85894edd4ef07fa9a33
Tomasz Pęczek <tpeczek@gmail.com>
2025-08-20 09:41:38 +0200

    [WASI p2] Adding .NET-based WASI HTTP component

---
+++ src/Demo.Wasi.Http/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Text;
+using ProxyWorld.wit.imports.wasi.http.v0_2_6;
+using ProxyWorld.wit.imports.wasi.io.v0_2_6;
+
+namespace ProxyWorld.wit.exports.wasi.http.v0_2_6;
+
+public class IncomingHandlerImpl : IIncomingHandler
+{
+    public static void Handle(ITypes.IncomingRequest request, ITypes.ResponseOutparam responseOut)
+    {
+        byte[] content = Encoding.ASCII.GetBytes("-- Demo.Wasi.Http --");
+
+        var headers = new List<(string, byte[])> {
+            ("Content-Type", Encoding.ASCII.GetBytes("text/plain")),
+	        ("Content-Length", Encoding.ASCII.GetBytes(content.Length.ToString()))
+	    };
+
+        ITypes.OutgoingResponse response = new ITypes.OutgoingResponse(ITypes.Fields.FromList(headers));
+	    ITypes.ResponseOutparam.Set(responseOut, Result<ITypes.OutgoingResponse, ITypes.ErrorCode>.Ok(response));
+        
+        ITypes.OutgoingBody responseBody = response.Body();
+	    using (IStreams.OutputStream responseBodyStream = responseBody.Write())
+        {
+            responseBodyStream.BlockingWriteAndFlush(content);
+        }
+        ITypes.OutgoingBody.Finish(responseBody, null);
+    }
+}
