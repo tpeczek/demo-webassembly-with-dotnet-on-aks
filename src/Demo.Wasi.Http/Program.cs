739962b9557024404400d7dced3696be0e8b27d6
Tomasz Pęczek <tpeczek@gmail.com>
2025-08-21 15:04:24 +0200

    [wasmCloud] Start wasmCloud host

--- src/Demo.Wasi.Http/Program.cs
+++ src/Demo.Wasi.Http/Program.cs
@@ -1,8 +1,8 @@
 ﻿using System.Text;
-using ProxyWorld.wit.imports.wasi.http.v0_2_6;
-using ProxyWorld.wit.imports.wasi.io.v0_2_6;
+using ProxyWorld.wit.imports.wasi.http.v0_2_0;
+using ProxyWorld.wit.imports.wasi.io.v0_2_0;
 
-namespace ProxyWorld.wit.exports.wasi.http.v0_2_6;
+namespace ProxyWorld.wit.exports.wasi.http.v0_2_0;
 
 public class IncomingHandlerImpl : IIncomingHandler
 {
